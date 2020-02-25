5b01b66d8e4ac218cb6ebf5655a4cfdd12dc58f4
Selva <writewaythinking@gmail.com>
2020-02-25 17:06:26 +0530

    Update controller

--- src/main/java/com/chainsys/controller/IndexController.java
+++ src/main/java/com/chainsys/controller/IndexController.java
@@ -1,11 +1,14 @@
-package controller;
+package com.chainsys.controller;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class IndexController {
 	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
 	@RequestMapping("/")
 	public String index() {
 		logger.info("this is a info message");
